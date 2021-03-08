20fbc517c3fe7e760fe7d438f465f8aaab63ca6e
MyTraining2021 <coolviji84@gmail.com>
2021-03-08 15:22:40 +0530

    My first Commit

---
+++ demoapp/src/main/java/org/cap/boot/MainClass.java
@@ -0,0 +1,15 @@
+package org.cap.boot;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		
+		
+		int[] myarr= {12,45,67,89,23};
+		
+		for(int n:myarr)
+			System.out.println(n);
+
+	}
+
+}
