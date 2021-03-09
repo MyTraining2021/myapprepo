4be27dd985446b7847e4301c5f0507182eabf15f
MyTraining2021 <coolviji84@gmail.com>
2021-03-09 14:34:22 +0530

    commit all

--- demoapp/src/main/java/org/cap/boot/SalesClass.java
+++ demoapp/src/main/java/org/cap/boot/SalesClass.java
@@ -9,10 +9,14 @@ public class SalesClass {
 	
 	public void display() {
 	
+		int ans=1;
 		
 		System.out.println("***********integer list*****************");
-		for(int i=0;i<list.size();i++)
+		for(int i=0;i<list.size();i++) {
+			ans*=list.get(i);
 			System.out.println(list.get(i));
+		}
+		System.out.println("Answer:" + ans);
 	}
 	
 	
