1a9246d3a0c9b2aa32d127235e0425457504ac33
MyTraining2021 <coolviji84@gmail.com>
2021-03-09 14:26:53 +0530

    commit with normal for loop

--- demoapp/src/main/java/org/cap/boot/SalesClass.java
+++ demoapp/src/main/java/org/cap/boot/SalesClass.java
@@ -11,8 +11,8 @@ public class SalesClass {
 	
 		
 		System.out.println("***********integer list*****************");
-		for(int num:list)
-			System.out.println(num);
+		for(int i=0;i<list.size();i++)
+			System.out.println(list.get(i));
 	}
 	
 	
